20418c4f16f8a2ac5975160819bc32d1786d2411
suyeol <suyeol@SUYEOL5544>
2024-01-31 14:23:18 -0700

    Comments in

--- Tic-Tac-Woah/Program.cs
+++ Tic-Tac-Woah/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+Console.WriteLine("Welcome to the Game! Hope you enjoy.");
+
+// Welcome the user to the game
+// Create a game board array to store the players’ choices
+// Ask each player in turn for their choice and update the game board array
+// Print the board by calling the method in the supporting class
+
+/* 
+ * Check for a winner by calling the method in the supporting class, and notify the players
+ * when a win has occurred and which player won the game
+*/
+
 namespace Tic_Tac_Woah
 {
     internal class Program
     {
-        // Welcome the user to the game
-        Console.WriteLine("Welcome to the Game!");
-        Console.WriteLine("Su made a change!")
+
+
     }
 }
+    
