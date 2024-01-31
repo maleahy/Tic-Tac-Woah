bd0609568daa7ee99390b5304b1fee451535ce39
maleahy <maleahyee@gmail.com>
2024-01-31 13:58:01 -0700

    program

--- Tic-Tac-Woah/Program.cs
+++ Tic-Tac-Woah/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Woah
+{
+    internal class Program
+    {
+        // Welcome the user to the game
+        Console.WriteLine("Welcome to the Game!");
+
+    }
+}
