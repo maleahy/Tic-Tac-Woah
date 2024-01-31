a73ec0ace47a2fda31991f211ced5ffb5d29cc36
maleahy <maleahyee@gmail.com>
2024-01-31 14:33:16 -0700

    test

--- Tic-Tac-Woah/Program.cs
+++ Tic-Tac-Woah/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Tic_Tac_Woah
 {
@@ -13,10 +15,11 @@ namespace Tic_Tac_Woah
             // Welcome the user to the game
             Console.WriteLine("Welcome to the Game!");
 
-          
-        }
-        // Welcome the user to the game
-        Console.WriteLine("Welcome to the Game!");
-        Console.WriteLine("Su made a change!")
+            //Create a game board array to store the players’ choices
+            //Ask each player in turn for their choice and update the game board array
+            //Print the board by calling the method in the supporting class
+            //Check for a winner by calling the method in the supporting class, and notify the players when a win has occurred and which player won the game
+
     }
+}
 }
